fba422c9e10889026a42913c2ebfec9acfd2aeca
petwill <sunfanyun@gmail.com>
2018-08-15 13:12:33 +0800

    update go.sh

--- go.sh
+++ go.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -ex
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/willy/.mujoco/mjpro150/bin
-python main.py --env-name "HalfCheetah-v2" --num-stack 1 --num-frames 100000000 --max-grad-norm 100000000000 --log-dir ./tmpp --scale-interval 2000
+
+python main.py --env-name "HalfCheetah-v2"  \
+               --num-stack 1  \
+               --num-frames 100000000  \
+               --max-grad-norm 100000000000  \
+               --log-dir ./tmp$1 \
+               --scale-interval $1 \
+               --plot-title  halfcheetah-$1 \
+               --saturation-log tmp$1.sat
 
