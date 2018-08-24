1a5ee4927a3dc97470fc8e06e0bf4f82c8401550
petwill <sunfanyun@gmail.com>
2018-08-24 23:43:33 +0800

    update script

--- go.sh
+++ go.sh
@@ -3,22 +3,21 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/willy/.mujoco/mjpro150/bin
 
 env=$1
-network_size=$2
-scale_interval=$3
-scale_threshold=$4
-network_ratio=$5
-r=$6
+network_size=64
+scale_interval=1000000000000000
+scale_threshold=2.
+network_ratio=1.
+r=$2
+seed=$3
 
-dir=$env-network_$network_size-network_ratio_$network_ratio-reward_scaling-$r
+dir=$env-network_$network_size-network_ratio_$network_ratio-reward_scaling-$r-seed_$seed
 
 python main.py --env-name "$env"  \
                --num-stack 1  \
                --hidden_size $network_size \
-               --num-frames 100000000  \
                --log-dir ./$dir \
                --scale-interval $scale_interval \
                --scale-threshold $2 \
                --plot-title  $dir \
-               --max-grad-norm 100000000000  \
                --saturation-log $dir/$dir.sat \
-               --network-ratio $network_ratio  --reward-scaling $r
+               --network-ratio $network_ratio  --reward-scaling $r --seed $seed
--- plot.py
+++ plot.py
@@ -10,20 +10,22 @@ import numpy as np
 import seaborn as sns; sns.set()
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 import pandas as pd
 
 from pylab import rcParams
 print(rcParams['figure.figsize'])
 rcParams['figure.figsize'] = 8, 4
 markers = ['o', 'v', 's', '^', 'D']
+legend_title = 'learning rate'
 
 
-def plot_reward(dir_names, legends, title=None, num=None, shade=False):
+def plot_reward(dir_names, legends, title=None, num=None, shade=True):
 
     sns.color_palette('hls', 10)
     x_label = 'million frames'
     y_label = 'cumulative reward'
-    legend_title = hue = 'reward scaling'
+    hue = legend_title
 
     dfss = []
     for idx, dir_name in enumerate(dir_names):
@@ -37,19 +39,19 @@ def plot_reward(dir_names, legends, title=None, num=None, shade=False):
 
         print(dir_name, num)
         if shade:
-            dfs = []
             for f in files:
-                res = pd.read_csv(f, skiprows=1)['r'].values[:minlen]
-                res = pd.rolling_mean(res, window=500)
+                tmpdf =pd.read_csv(f, skiprows=1) 
+
+                x = tmpdf['l'].cumsum().values
+                y = pd.rolling_mean(tmpdf['r'].values, window=100)
+
+                tmpf = interp1d(x, y)
 
                 df = pd.DataFrame()
-                df[y_label] = res
-                df[x_label] = np.arange(res.shape[0])
+                df[x_label] = np.arange(1000, num, 1000)
+                df[y_label] = tmpf(df[x_label].values)
                 df[hue] = legends[idx]
-                dfs.append(df)
-
-            dfs = pd.concat(dfs)
-            dfss.append(dfs)
+                dfss.append(df)
         else:
             for f in files[:1]:
                 tmpdf = pd.read_csv(f, skiprows=1)  
@@ -57,7 +59,7 @@ def plot_reward(dir_names, legends, title=None, num=None, shade=False):
                 # df[y_label] = pd.rolling_mean(tmpdf['r'].values[:minlen], window=500)
                 # df[x_label] = tmpdf['l'].cumsum().values[:minlen]
 
-                y = pd.rolling_mean(tmpdf['r'], window=500).values
+                y = pd.rolling_mean(tmpdf['r'], window=100).values
                 x = tmpdf['l'] = tmpdf['l'].cumsum().values
                 
 
@@ -70,15 +72,17 @@ def plot_reward(dir_names, legends, title=None, num=None, shade=False):
                 plt.plot(x, y, label=legends[idx])
 
 
-    # df = pd.concat(dfss)
+    if shade:
+        df = pd.concat(dfss)
     # df = df.append({x_label:0, y_label:0, hue:'dummy'}, ignore_index=True)
-    # g = sns.lineplot(x=x_label, y=y_label, hue=hue, data=df, style=hue)
+        g = sns.lineplot(x=x_label, y=y_label, hue=hue, data=df, style=hue)
     # g = sns.lineplot(x=x_label, y=y_label, hue=hue, style=hue, markers=['o', 'v', 's', 'p'], dashes=False, data=df, markevery=100)
     # g = sns.lineplot(x=x_label, y=y_label, hue=hue, style=hue, markers=True, dashes=False, data=df, markevery=100)
 
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.gcf().subplots_adjust(bottom=0.15)
+    else:
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.gcf().subplots_adjust(bottom=0.15)
 
     ax = plt.gca()
     box = ax.get_position()
@@ -109,16 +113,15 @@ def plot_saturation(fnames, legends, title=None, num=None):
     fnames = ['{}/{}.sat'.format(x, os.path.basename(x)) for x in fnames]
     x_label = 'million frames'
     y_label = 'PDRR'
-    legend_title= 'reward scaling'
     def go(layeridx):
         for idx, fname in enumerate(fnames):
             df = pd.read_csv(fname, header=None)
             if layeridx == 2:
                 y = pd.rolling_mean(df.iloc[:, 1] ,window=500)[:num]
-                plt.plot(np.arange(y.shape[0])*10/1e6 * 5, y, label='{}-relu{}'.format(legends[idx], layeridx))
+                plt.plot(np.arange(y.shape[0])*10/1e6 * 5, y, label=legends[idx])
             else:
                 y = pd.rolling_mean(df.iloc[:, 0] ,window=500)[:num]
-                plt.plot(np.arange(y.shape[0])*10/1e6 * 5, y, label='{}-relu{}'.format(legends[idx], layeridx))
+                plt.plot(np.arange(y.shape[0])*10/1e6 * 5, y, label=legends[idx])
 
         if title is not None:
             plt.title(title)
@@ -136,6 +139,7 @@ def plot_saturation(fnames, legends, title=None, num=None):
         plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., title=legend_title)
 
         # plt.tight_layout()
+        plt.title('relu-{}'.format(layeridx))
         plt.savefig(f'relu{layeridx}')
         plt.close()
     go(2)
@@ -151,10 +155,53 @@ def plot_dirs(dirs, legends=None):
 
 if __name__ == '__main__':
 
+    """ learning rate experiment """
+    pref = '../Hopper-v2/Hopper-v2-network_64-network_ratio_.5-lr_'
+    lrs = ['1e-5_', '1e-4_', '7e-4_', '1e-3_', '7e-3_' ]
+    dirs = ['{}{}'.format(pref, lr)[:-1] for lr in lrs]
+    plot_reward(dirs, lrs, num=1200000) 
+    plot_saturation(dirs, lrs, num=24000)
+    
+    # """ reward scalling experiment
+    # Hopper
+    """
+    legend_title = 'reward scale'
+    pref = '../Hopper-v2/Hopper-v2-network_64-network_ratio_.5-reward_scaling_'
+    scales =  ['.5', '.75', '1', '5', '10', '30', '100']
+    dirs = [f'{pref}{x}' for x in scales]
+    legends = scales
+    plot_reward(dirs, legends, num=1500000)
+    plot_saturation(dirs, legends, num=30000)
+    """
+    # HalfChetah-v2 
+    """
+    legend_title = 'reward scale'
+    pref = '../HalfCheetah-v2/HalfCheetah-v2-network_64-network_ratio_1.-reward_scaling-'
+    scales =  ['1.', '5', '10', '30', '50', '70']
+    dirs = [f'{pref}{x}' for x in scales]
+    legends = scales
+    plot_reward(dirs, legends, num=1500000)
+    plot_saturation(dirs, legends, num=30000)
+    """
+    # Swimmer-v2
+    """
+    legend_title = 'reward scale'
+    pref = 'Swimmer-v2-network_64-network_ratio_1.-reward_scaling-'
+    scales =  ['1.', '5', '10', '30', '50', '70']
+    dirs = [f'{pref}{x}' for x in scales]
+    legends = scales
+    plot_reward(dirs, legends, num=1500000)
+    plot_saturation(dirs, legends, num=30000)
+    """
+    # Walker2d-v2
+    legend_title = 'reward scale'
+    pref = 'Walker2d-v2-network_64-network_ratio_1.-reward_scaling-'
+    scales =  ['1.', '10', '30', '50']
+    dirs = [f'{pref}{x}' for x in scales]
+    legends = scales
+    plot_reward(dirs, legends, num=1500000, shade=True)
+    plot_saturation(dirs, legends, num=30000)
 
-    # Hopper different scaling interval experiment
-    # plot_reward(['../Hopper-v2/Hopper-v2-scale-5000', '../Hopper-v2/network_64', 'grad_clipping'], ['scale_5000', 'original', 'grad_clipping'], 2300)
-    # plot_saturation(['../Hopper-v2/Hopper-v2-scale-5000/Hopper-v2-scale-5000.sat', '../Hopper-v2/network_64/network_64.sat', 'grad_clipping.sat'], ['scale-5000', 'original', 'grad_clipping'], num=25000)
 
     ## Hopper network size saturation experiment
     # plot_reward(['./Hopper-v2-network_8', '../Hopper-v2/network_16', '../Hopper-v2/network_64', '../Hopper-v2/network_256', 'Hopper-v2-network_8-scale_thresh_.4'], ['network_8', 'network_16', 'network_64', 'network_256', 'network_8-scale_thresh_.2'])
@@ -247,26 +294,14 @@ if __name__ == '__main__':
     # plot_dirs(dirs)
     """
 
-    # pref = './Hopper-v2-network_64-network_ratio_.5-reward_scaling_'
-    # scales =  ['.5', '.75', '1', '5', '10', '30', '100']
+
+    # pref = './HalfCheetah-v2-network_64-network_ratio_1.-reward_scaling-'
+    # scales =  ['.75', '5', '30']
     # dirs = [f'{pref}{x}' for x in scales]
     # legends = scales
     # plot_reward(dirs, legends, num=1500000)
     # plot_saturation(dirs, legends, num=30000)
 
-    pref = './HalfCheetah-v2-network_64-network_ratio_1.-reward_scaling-'
-    scales =  ['.75', '5', '30']
-    dirs = [f'{pref}{x}' for x in scales]
-    legends = scales
-    plot_reward(dirs, legends, num=1500000)
-    plot_saturation(dirs, legends, num=30000)
-
-    # pref = 'Hopper-v2-network_64-network_ratio_.5-lr_'
-    # lrs = ['1e-5', '1e-4', '7e-4', '1e-3', '7e-3' ]
-    # dirs = ['{}{}'.format(pref, lr) for lr in lrs]
-    # dirs[2] = 'Hopper-v2-network_64-network_ratio_.5-reward_scaling_1'
-    # plot_reward(dirs, lrs, num=1200000) 
-    # plot_saturation(dirs, lrs, num=25000)
 
     # dirs = ['Hopper-v2-network_64-network_ratio_.1-scaling_leaky', 'Hopper-v2-network_64-network_ratio_.1-rmsprop_rescaling', 'Hopper-v2-network_64-weight_.1']
     # legends = ['leaky + out method', 'our method', 'original']
